f3dcfe3a3f007581c2cad2ad2c1b60dab8af25ba
Ricardo <163490245+RickV22@users.noreply.github.com>
2024-11-15 16:01:23 -0500

    Update index.js

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -181,7 +181,7 @@ function guardarCambios(id) {
         });
 }
 
-
+/*
 function eliminarHeroe(id) {
     if (confirm('¿Está seguro de eliminar este héroe?')) {
         axios.delete(`https://heroes-tdx5.vercel.app/api/heroes/${id}`)
