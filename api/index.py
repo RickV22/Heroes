de7828bdb62c246ab5ecaf74aaac5c76f2ce43e5
Ricardo <163490245+RickV22@users.noreply.github.com>
2024-11-26 12:22:59 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -70,7 +70,7 @@ def update_hero(id):
     SET nombre = %s, habilidad = %s, compania = %s, genero = %s, descripcion = %s, imagen = %s
     WHERE id = %s
     """
-    cursor.execute(sql, (updated_data['nombre'], updated_data['habilidad'], updated_data['compania'], updated_data['genero'], updated_data['descripcion'], id)), update_data['imagen']
+    cursor.execute(sql, (updated_data['nombre'], updated_data['habilidad'], updated_data['compania'], updated_data['genero'], updated_data['descripcion'], id, update_data['imagen']))
     connection.commit()
     cursor.close()
     connection.close()
